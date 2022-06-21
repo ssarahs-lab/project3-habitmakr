044b0add59eb017e5cc2fd7dbaeb02ce76122aa8
alexmercer <alex.mercer6@hotmail.com>
2022-06-22 09:30:53 +1000

    fixed header bug and hid calender to work on it

--- client/js/components/testCalender.js
+++ client/js/components/testCalender.js
@@ -1,11 +1,13 @@
-
-var calendar = new tui.Calendar('#calendar', {
-    defaultView: 'month',
-    taskView: true,
-    template: {
-      monthDayname: function(dayname) {
-        return '<span class="calendar-week-dayname-name">' + dayname.label + '</span>';
+export function renderCalender() {
+    var calendar = new tui.Calendar('#calendar', {
+      defaultView: 'month',
+      taskView: true,
+      template: {
+        monthDayname: function(dayname) {
+          return '<span class="calendar-week-dayname-name">' + dayname.label + '</span>';
+        }
       }
-    }
-  });
+    });
+
+}
 
