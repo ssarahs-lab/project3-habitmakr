6b53b6e5e612fe9b119b5eaca4037cbff4e1add6
alexmercer <alex.mercer6@hotmail.com>
2022-06-22 10:12:37 +1000

    cleaning up cal func

--- client/js/components/testCalender.js
+++ client/js/components/testCalender.js
@@ -1,44 +1,12 @@
-function renderCalender() {
-    var calendar = new tui.Calendar('#calendar', {
-      defaultView: 'month',
-      taskView: true,
-      template: {
-        monthDayname: function(dayname) {
-          return '<span class="calendar-week-dayname-name">' + dayname.label + '</span>';
-        }
+export function renderCalender() {
+  var calendar = new tui.Calendar('#calendar', {
+    defaultView: 'month',
+    taskView: true,
+    template: {
+      monthDayname: function(dayname) {
+        return '<span class="calendar-week-dayname-name">' + dayname.label + '</span>';
       }
+    }
   });
 
-
-  axios.get('/api/userHabits')
-    .then((response) => {
-      console.log(response)
-      // calendar.createSchedules([
-      //   // {
-      //   //     id: '1',
-      //   //     calendarId: '1',
-      //   //     title: 'my schedule',
-      //   //     category: 'time',
-      //   //     dueDateClass: '',
-      //   //     start: new Date(),
-      //   //     end: new Date()
-      //   // },
-      //   // {
-      //   //     id: '2',
-      //   //     calendarId: '1',
-      //   //     title: 'second schedule',
-      //   //     category: 'time',
-      //   //     dueDateClass: '',
-      //   //     start: '2018-01-18T17:30:00+09:00',
-      //   //     end: '2018-01-19T17:31:00+09:00'
-      //   // }
-        
-      // ]);
-      
-    })
-
-
-  calendar.render();
 }
-
-renderCalender()
